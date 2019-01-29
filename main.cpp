f3f2180718a70c35c345db8a878da7ae51ad7914
Kristoffer D <KristofferD@users.noreply.github.com>
2019-01-29 10:30:42 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
-//#include <cstdio> prinft would print it as wanted.
-//#include <string> //Could be used to verify argv is correct filetype.
+#include <string> //Could be used to verify argv is correct filetype.
 
 /**
  * The program reads numbers from a file and prints the sum of them
@@ -11,38 +10,41 @@
  * @return      [description]
  */
 void expandArray(int *&arr, int &arraySize);
-//bool has_suffix(const std::string &str, const std::string &suffix);
-//bool validFile(int argc, char** argv, string &readFromFile);
+bool has_suffix(const std::ifstream &str, const std::string &suffix);
+bool validFile(int argc, char** argv, std::ifstream &readFromFile);
 int main(int argc, const char * argv[])
 {
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     std::ifstream myReadFile;
-    /*if(!validFile(argc, argv, string &readFromFile){return -1;} Validate fileformat and that the program got any argv at all if not exit with -1
-else{*/
-    myReadFile.open(argv[1]);
-    int arraySize = 3;
-    int *numbers = new int[arraySize];
-    int nrOfNumbers = 0;
-    int inputFromFile = 0; //Better description of variable then a
-    int sum = 0;
-    while (myReadFile >> inputFromFile)
-    {
-        if(nrOfNumbers == arraySize){
-          expandArray(numbers, arraySize);
+    if(!validFile(argc, argv, std::ifstream myReadFile){return -1;} //Validate fileformat and that the program got any argv at all if not exit with -1
+    else{
+      myReadFile.open(argv[1]);
+      int arraySize = 3;
+      int *numbers = new int[arraySize];
+      int nrOfNumbers = 0;
+      int inputFromFile = 0; //Better description of variable then a
+      int sum = 0;
+      while (myReadFile >> inputFromFile)
+        {
+          if(nrOfNumbers == arraySize){
+            expandArray(numbers, arraySize);
+            }
+          numbers[nrOfNumbers] = inputFromFile;
+          nrOfNumbers++;
+          sum += inputFromFile;
         }
-        numbers[nrOfNumbers] = inputFromFile;
-        nrOfNumbers++;
-        sum += inputFromFile;
-    }
-    myReadFile.close();
-    for(int i = 0; i < nrOfNumbers; i++)
-    {
-      if(numbers[i] < (sum/nrOfNumbers)){
-        float printValue = numbers[i];
-        printf("%.3f", printValue);
-      }
-    }
+      myReadFile.close();
+      for(int i = 0; i < nrOfNumbers; i++)
+        {
+          if(numbers[i] < (sum/nrOfNumbers)){
+            float printValue = numbers[i];
+            printf("%.3f " printValue);
+          }
+        }
+
     delete[] numbers;
     return 0;
+  }
 }
 
 
@@ -56,7 +58,7 @@ void expandArray(int *&arr, int &arraySize){ //For performance it can be bad to 
   arr = temp;
   arraySize++;
 }
-/*bool validFile(int argc, char** argv, string &readFromFile) //Make sure it's a .txt file and not error because of incorect input file type.
+bool validFile(int argc, char** argv, std::ifstream &readFromFile) //Make sure it's a .txt file and not error because of incorect input file type.
 {
         if (argc > 1) {
                 readFromFile = argv[1];
@@ -67,4 +69,4 @@ void expandArray(int *&arr, int &arraySize){ //For performance it can be bad to 
         else{
               	return true;
         }
-}*/
+}
